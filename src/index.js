dffd20a68fdfd2765bc038d6aa051d4a46df771b
Marcus Hobdy <hobdy.marcusjr@gmail.com>
2018-09-08 00:21:16 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+//redux-promise is middleware that will
+//stop a promise that is see's comeing from action's payload and heading 
+//to a reducer and resolve that promise befor updating the payload and sending it to the reducer
+import ReduxPromise from 'redux-promise';
 
 import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
